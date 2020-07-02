277e88d93bf0b07b061e9af5253b2e6efff401f0
Gabriel Vacari <gabriel_vacari@outlook.com>
2020-07-02 17:52:07 -0300

    remove console.log

--- dev/backend/src/services/socket.js
+++ dev/backend/src/services/socket.js
@@ -3,13 +3,10 @@ const heroisForOccurence = require('./resolveOccurrence');
 require('dotenv').config();
 
 const io = socket(process.env.SOCKET_URL);
-io.on('connect', () => {
-  console.log('socket connected');
-});
+io.on('connect');
 
 function listenEvent() {
   io.on('occurrence', (data) => {
-    console.log(data);
     heroisForOccurence(data);
   });
 }
