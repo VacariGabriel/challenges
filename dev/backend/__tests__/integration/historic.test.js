44618f7bb548b1149fe3577d6583b841dd30ba47
Gabriel Vacari <gabriel_vacari@outlook.com>
2020-07-02 19:30:32 -0300

    create tests for historic

---
+++ dev/backend/__tests__/integration/historic.test.js
@@ -0,0 +1,22 @@
+const request = require('supertest');
+const app = require('../../src/server');
+const generateToken = require('../../src/utils/token');
+
+const LOGIN = 'flashzin';
+
+describe('Tests for historic', () => {
+  it("shouldn't access authorized routes without token", async () => {
+    const response = await request(app).get('/authorized/historico');
+
+    expect(response.status).toBe(401);
+  });
+
+  it('should get historic', async () => {
+    const response = await request(app)
+      .get('/authorized/historico')
+      .set('Authorization', `Bearer ${generateToken(LOGIN)}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body[0]).toHaveProperty('hero_name');
+  });
+});
