d8f283ba6a248e4da994acc6db49e10e80b5e30f
Gabriel Vacari <gabriel_vacari@outlook.com>
2020-07-02 21:01:47 -0300

    create historic seed

---
+++ dev/backend/src/database/seeds/create_historic.js
@@ -0,0 +1,32 @@
+async function seed(knex) {
+  await knex('historic').insert([
+    {
+      hero_name: 'Flash',
+      hero_rank: 'S',
+      threat_name: 'Very Strong Threat',
+      threat_rank: 'God',
+    },
+    {
+      hero_name: 'Flash',
+      hero_rank: 'A',
+      threat_name: 'Medium Threat',
+      threat_rank: 'God',
+    },
+    {
+      hero_name: 'Flash',
+      hero_rank: 'B',
+      threat_name: 'Weak Threat',
+      threat_rank: 'God',
+    },
+    {
+      hero_name: 'Flash',
+      hero_rank: 'C',
+      threat_name: 'Very Weak Threat',
+      threat_rank: 'God',
+    },
+  ]);
+}
+
+module.exports = {
+  seed,
+};
